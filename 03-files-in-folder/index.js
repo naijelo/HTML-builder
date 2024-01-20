5a8fb646f58113b31b768efda675db59aa008b5e
naijelo <naijel.a@gmail.com>
2024-01-21 00:45:56 +0300

    refactor: rework task3 implementation according to conditions

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,22 @@
 const fs = require("fs");
 const fsPromises = fs.promises;
 const path = require("path");
-const pathToFile = "./03-files-in-folder/secret-folder";
+const pathToFile = path.join(__dirname, "/secret-folder");
 
 fs.readdir(pathToFile, {withFileTypes: true}, (err, files) => {
   if (err)
     console.log(err);
   else { 
     files.forEach(file => {
-      const extName = path.extname(`${pathToFile}/${file.name}`).slice(1);
-      const name = file.name.split(".")[0];
-      (async () => {
-        try {
-          let stats = await fsPromises.stat( 
-            (`${pathToFile}/${file.name}`));
-          console.log(`${name} - ${extName} - ${stats.size}b`);
-        }
-        catch (error) {
-          console.log(error);
-        }
-      })();
+      if(!file.isDirectory()) {
+        const extName = path.extname(`${pathToFile}/${file.name}`).slice(1);
+        const name = file.name.split(".")[0];
+        (async () => {
+            let stats = await fsPromises.stat( 
+              (`${pathToFile}/${file.name}`));
+            console.log(`${name} - ${extName} - ${stats.size}b`);
+        })();
+      }
     });
   };
 });
